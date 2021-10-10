296d6e731c66fa911b279ecd7c121dd513b5654e
Nathan Haile <hailenathan0@gmail.com>
2021-10-10 13:21:30 -0400

    edits

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,6 +1,7 @@
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson";
-
+// get request to query URL
 d3.json(queryUrl).then(function(data){
+    //send response of data to createFeatures function
     createFeatures(data.features);
     console.log(data)
 
@@ -8,9 +9,32 @@ d3.json(queryUrl).then(function(data){
 
 function createFeatures(earthquakeData) {
 
-
+//Define faunction to run once for each feature in the features array.
+//Give each feature a popup that describes the place and time of earthquake
 
     function onEachFeature(feature, layer){
         layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
+    }
+
+    var earthquakes = L.geoJSON(earthquakeData, {
+        onEachFeature: onEachFeature,
+        pointToLayer: function (feature, latlng) {
+            let radius = feature.properties.mag * 5;
+
+            return new L.CircleMarker(latlng, {
+                fillOpacity:0.75,
+                color: "white",
+                fillColor: getFillColor(feature.geometry.coordinates[2]),
+                radius: radius
+            });
+        },
+    });
+
+    createMap(earthquakes);
+}
+
+function getFillColor(depth) {
+    if (depth >= 90) {
+        return 
     }
 }
